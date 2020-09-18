e02aa13c75d3a3b9bc95897e34a3963887df1971
YangChaoqun <51304156+HappyThis@users.noreply.github.com>
2020-09-18 09:07:49 +0800

    去除URL硬编码

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from blog import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/article_detail/', views.ArticleDetail.as_view(), name='detail'),
+    path('article_detail/<int:pk>', views.ArticleDetail.as_view(), name='detail'),
 ]
